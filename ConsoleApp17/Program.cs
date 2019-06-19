463a4c0080c95f59a3f17acca1e0f89283a57e7a
jdraf <aajay78@hotmail.com>
2019-06-19 18:57:03 -0400

    Update Program.cs

--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,172 +22,172 @@ namespace ConsoleApp17
         private static void ReadTextFile(string path)
         {
             string string1 = ""; Decimal elaps1;
-           try
+            try
             {
-            string[] lines = File.ReadAllLines(path);
+                string[] lines = File.ReadAllLines(path);
 
             var list = new List<KeyValuePair<string, Decimal>>();
 
             var Listabove = new List<KeyValuePair<string, Decimal>>();
+           
+                for (int i = 0; i < lines.Count(); i++)
+                {
 
-            for (int i = 0; i < lines.Count(); i++)
-            {
+                    int result; int result1;
+                    int a = 0;
 
-                int result; int result1;
-                int a = 0;
+                    if (lines[i].Split(',')[0] == "start" && lines[i + 1].Split(',')[0] != "stop")
 
-                if (lines[i].Split(',')[0] == "start" && lines[i + 1].Split(',')[0] != "stop")
-                  
-                   Listabove.Add(new KeyValuePair<string, Decimal>(lines[i].Split(',')[1], Convert.ToDecimal(lines[i].Split(',')[2])));
+                        Listabove.Add(new KeyValuePair<string, Decimal>(lines[i].Split(',')[1], Convert.ToDecimal(lines[i].Split(',')[2])));
 
-                if (lines[i].Split(',')[0] == "stop")
+                    if (lines[i].Split(',')[0] == "stop")
 
+                    {
+                        if (lines[i - 1].Split(',')[0] != "stop")
+                        {
+                            var j = Convert.ToDecimal(lines[i - 1].Split(',')[2]);
+
+                            var j1 = Convert.ToDecimal(lines[i].Split(',')[1]);
+
+                            var elapstime = j1 - j;
+
+                            list.Add(new KeyValuePair<string, Decimal>(lines[i - 1].Split(',')[1], elapstime));
+
+                            // catch left over 
+
+                            if (lines[i + 1].Split(',')[0] == "stop")
                             {
-                                if(lines[i - 1].Split(',')[0] != "stop")
-                                { 
-                                    var j = Convert.ToDecimal(lines[i - 1].Split(',')[2]);
-                    
-                                    var j1 = Convert.ToDecimal(lines[i].Split(',')[1]);
+                                var k = Convert.ToDecimal(lines[i + 1].Split(',')[1]);
 
-                                     var elapstime = j1 - j;
+                                list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k - Listabove.Last().Value));
 
-                                 list.Add(new KeyValuePair<string, Decimal>(lines[i - 1].Split(',')[1], elapstime));
+                                Listabove.RemoveAt(Listabove.Count - 1);
+                                if (lines[i + 2].Split(',')[0] == "stop")
+                                {
+                                    var k2 = Convert.ToDecimal(lines[i + 2].Split(',')[1]);
 
-                                 // catch left over 
+                                    list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k2 - Listabove.Last().Value));
 
-                                    if (lines[i + 1].Split(',')[0] == "stop")
+                                    Listabove.RemoveAt(Listabove.Count - 1);
+
+                                    if ((i + 3) <= lines.Count() - 1)
                                     {
-                                        var k = Convert.ToDecimal(lines[i + 1].Split(',')[1]);
-                            
-                                        list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k - Listabove.Last().Value));
 
-                                        Listabove.RemoveAt(Listabove.Count - 1);
-                                        if (lines[i + 2].Split(',')[0] == "stop")
+                                        if (lines[i + 3].Split(',')[0] == "stop")
                                         {
-                                            var k2 = Convert.ToDecimal(lines[i + 2].Split(',')[1]);
+                                            var k3 = Convert.ToDecimal(lines[i + 3].Split(',')[1]);
 
-                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k2 - Listabove.Last().Value));
+                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k3 - Listabove.Last().Value));
 
                                             Listabove.RemoveAt(Listabove.Count - 1);
 
-                                            if ((i+3) <= lines.Count()-1)
-                                             {
+                                            if ((i + 4) <= lines.Count() - 1)
+                                            {
 
-                                                if (lines[i + 3].Split(',')[0] == "stop")
+                                                if (lines[i + 4].Split(',')[0] == "stop")
+                                                {
+                                                    var k4 = Convert.ToDecimal(lines[i + 4].Split(',')[1]);
+
+                                                    list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k4 - Listabove.Last().Value));
+
+                                                    Listabove.RemoveAt(Listabove.Count - 1);
+
+                                                    if ((i + 5) <= lines.Count() - 1)
                                                     {
-                                                        var k3 = Convert.ToDecimal(lines[i + 3].Split(',')[1]);
 
-                                                        list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k3 - Listabove.Last().Value));
+                                                        if (lines[i + 5].Split(',')[0] == "stop")
+                                                        {
+                                                            var k5 = Convert.ToDecimal(lines[i + 5].Split(',')[1]);
 
-                                                        Listabove.RemoveAt(Listabove.Count - 1);
+                                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k5 - Listabove.Last().Value));
 
-                                                            if ((i + 4) <= lines.Count() - 1)
+                                                            Listabove.RemoveAt(Listabove.Count - 1);
+
+                                                            if ((i + 6) <= lines.Count() - 1)
                                                             {
 
-                                                                if (lines[i + 4].Split(',')[0] == "stop")
+                                                                if (lines[i + 6].Split(',')[0] == "stop")
                                                                 {
-                                                                    var k4 = Convert.ToDecimal(lines[i + 4].Split(',')[1]);
+                                                                    var k6 = Convert.ToDecimal(lines[i + 6].Split(',')[1]);
 
-                                                                    list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k4 - Listabove.Last().Value));
+                                                                    list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k6 - Listabove.Last().Value));
 
                                                                     Listabove.RemoveAt(Listabove.Count - 1);
 
-                                                                    if ((i + 5) <= lines.Count() - 1)
+                                                                    if ((i + 7) <= lines.Count() - 1)
                                                                     {
 
-                                                                        if (lines[i + 5].Split(',')[0] == "stop")
+                                                                        if (lines[i + 7].Split(',')[0] == "stop")
                                                                         {
-                                                                            var k5 = Convert.ToDecimal(lines[i + 5].Split(',')[1]);
+                                                                            var k7 = Convert.ToDecimal(lines[i + 7].Split(',')[1]);
 
-                                                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k5 - Listabove.Last().Value));
+                                                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k7 - Listabove.Last().Value));
 
                                                                             Listabove.RemoveAt(Listabove.Count - 1);
 
-                                                                        if ((i + 6) <= lines.Count() - 1)
-                                                                        {
-
-                                                                            if (lines[i + 6].Split(',')[0] == "stop")
+                                                                            if ((i + 8) <= lines.Count() - 1)
                                                                             {
-                                                                                var k6 = Convert.ToDecimal(lines[i + 6].Split(',')[1]);
 
-                                                                                list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k6 - Listabove.Last().Value));
-
-                                                                                Listabove.RemoveAt(Listabove.Count - 1);
-
-                                                                                if ((i + 7) <= lines.Count() - 1)
+                                                                                if (lines[i + 8].Split(',')[0] == "stop")
                                                                                 {
+                                                                                    var k8 = Convert.ToDecimal(lines[i + 8].Split(',')[1]);
 
-                                                                                    if (lines[i + 7].Split(',')[0] == "stop")
-                                                                                    {
-                                                                                        var k7 = Convert.ToDecimal(lines[i + 7].Split(',')[1]);
+                                                                                    list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k8 - Listabove.Last().Value));
 
-                                                                                        list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k7 - Listabove.Last().Value));
-
-                                                                                        Listabove.RemoveAt(Listabove.Count - 1);
-
-                                                                                    if ((i + 8) <= lines.Count() - 1)
+                                                                                    Listabove.RemoveAt(Listabove.Count - 1);
+                                                                                    if ((i + 9) <= lines.Count() - 1)
                                                                                     {
 
-                                                                                        if (lines[i + 8].Split(',')[0] == "stop")
+                                                                                        if (lines[i + 9].Split(',')[0] == "stop")
                                                                                         {
-                                                                                            var k8 = Convert.ToDecimal(lines[i + 8].Split(',')[1]);
+                                                                                            var k9 = Convert.ToDecimal(lines[i + 9].Split(',')[1]);
 
-                                                                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k8 - Listabove.Last().Value));
+                                                                                            list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k9 - Listabove.Last().Value));
 
                                                                                             Listabove.RemoveAt(Listabove.Count - 1);
-                                                                                        if ((i + 9) <= lines.Count() - 1)
-                                                                                        {
 
-                                                                                            if (lines[i + 9].Split(',')[0] == "stop")
-                                                                                            {
-                                                                                                var k9 = Convert.ToDecimal(lines[i + 9].Split(',')[1]);
-
-                                                                                                list.Add(new KeyValuePair<string, Decimal>(Listabove.Last().Key, k9 - Listabove.Last().Value));
-
-                                                                                                Listabove.RemoveAt(Listabove.Count - 1);
-
-                                                                                            }
                                                                                         }
-
-                                                                            }
                                                                                     }
 
-                                                                            }
                                                                                 }
+                                                                            }
 
-                                                            }
                                                                         }
-
-                                                    }
                                                                     }
 
-                                            }
+                                                                }
                                                             }
+
+                                                        }
                                                     }
+
+                                                }
                                             }
-
-
+                                        }
                                     }
-                                }
-                        
 
-                          }
-                               
+
+                                }
+                            }
+
+
+                        }
+
+                    }
+
+
                 }
 
-               
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine($" {item.Key} :     {item.Value}");
+
+                }
+                Console.ReadLine();
             }
-
-
-            foreach (var item in list)
-            {
-                Console.WriteLine($" {item.Key} :     {item.Value}");
-                
-            }
-            Console.ReadLine();
-
-        }
-             catch (FileNotFoundException ef)
+           
+            catch (FileNotFoundException ef)
             {
                 // FileNotFoundExceptions are handled here.
             }
@@ -203,6 +203,7 @@ namespace ConsoleApp17
             {
                 Console.WriteLine("{0} Exception caught.", e);
             }
+        }
 
     }
 }
